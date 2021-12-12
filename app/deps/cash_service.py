2499449f0ac215b90862a1f2f33344a059b46caa
hwc9169 <hwc9169@gmail.com>
2021-12-12 19:47:06 +0900

    consumer url 수정

--- app/deps/cash_service.py
+++ app/deps/cash_service.py
@@ -10,7 +10,7 @@ class CashService():
     async def create_consumer(self, token):
         headers={"Authorization": f"Bearer {token}"}
         async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.post(f"{settings.CASH_SERVICE_BASE_URL}/api/v1/cash/consumer") as resp:
+            async with session.post(f"{settings.CASH_SERVICE_BASE_URL}/api/v1/cash/consumers") as resp:
                 if resp.status != 200:
                     raise HTTPException(status_code=resp.status, detail=await resp.text())
 
